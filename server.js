a6c1031629da4fce6b2e88e459c4704659d7d9b7
Александр <nikol.alex06@mail.ru>
2026-01-22 19:28:32 +0300

    feat: from actual

---
+++ server.js
@@ -0,0 +1,44 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: parseInt(process.env.SMTP_PORT),
+    secure: process.env.SMTP_SECURE === 'true',
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASSWORD,
+    },
+});
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/health', (req, res) => {
+  res.send('Server is running');
+});
+
+// Роут для отправки email
+app.post('/send-contacts', async (req, res) => {
+  const {  } = req.body;
+
+  try {
+    await transporter.sendMail({
+      from: `<${process.env.SMTP_USER}>`,
+      to: process.env.EMAIL_TO,
+      subject: "Контакты с сайта",
+      text: JSON.stringify(req.body),
+    });
+    res.status(200).send('Email sent successfully');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error sending email');
+  }
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
